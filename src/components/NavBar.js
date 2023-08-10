b89b78adab1268cf3ce472e4e85cb7175c6944bc
Leandro Elverdi <leandroelverdi@gmail.com>
2023-08-09 23:48:02 -0300

    create itemListContainer component

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,25 +1,35 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import cart from "../assets/cart.png";
 
 export const NavBar = () => {
   return (
     <>
-      <nav className="flex items-center justify-between w-100 h-20 px-5">
-        <a href="#home" className="flex items-center gap-1"><img src="logo.png" alt="Logo" className="w-[80px]"></img>Tienda</a>
-        <ul className="flex gap-4 [&>li>a]:rounded [&>li>a]:px-4 [&>li>a]:py-2">
-          <li>
-            <a href="#home" className="hover:bg-blue-300">Inicio</a>
-          </li>
-          <li>
-            <a href="#products" className="hover:bg-blue-300">Productos</a>
-          </li>
-          <li>
-            <a href="#about-us" className="hover:bg-blue-300">Sobre Nosotros</a>
-          </li>
-        </ul>
-        <a href="#cart">
-          <FontAwesomeIcon icon="fa-solid fa-cart-shopping" /> 0
+      <header className="flex justify-between px-5">
+        <a href="#home" className="flex items-center gap-1">
+          <img src="logo.png" alt="Logo" className="w-[80px]"></img>Tienda
         </a>
-      </nav>
+        <nav className="flex items-center justify-between w-100 h-20">
+          <ul className="flex gap-4 [&>li>a]:rounded [&>li>a]:px-4 [&>li>a]:py-2">
+            <li>
+              <a href="#home" className="hover:bg-blue-300">
+                Inicio
+              </a>
+            </li>
+            <li>
+              <a href="#products" className="hover:bg-blue-300">
+                Productos
+              </a>
+            </li>
+            <li>
+              <a href="#about-us" className="hover:bg-blue-300">
+                Sobre Nosotros
+              </a>
+            </li>
+          </ul>
+        </nav>
+        <div className="flex items-center">
+          <img src={cart} alt="Cart" className="w-[25px] h-[25px]" /> 0
+        </div>
+      </header>
     </>
   );
 };
---
+++ src/components/itemListContainer.jsx
@@ -0,0 +1,3 @@
+export const itemListContainer = () => {
+  return <div></div>
+}
